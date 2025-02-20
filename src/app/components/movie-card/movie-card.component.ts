844865ba573caeabb4469b639c7a9d22a640ade5
Adrián Maza <adri_mv1@hotmail.com>
2025-02-20 19:01:20 +0100

    output & constructor

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-movie-card',
   imports: [],
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.css'
+  styleUrl: './movie-card.component.css',
 })
 export class MovieCardComponent {
   @Input()
   movie: any = {};
+
+  messageSent = output<string>();
+
+  @Output()
+  watchlistChange = new EventEmitter<number>();
+
+  sendMsg() {
+    this.messageSent.emit('Mensaje desde ' + this.movie.title);
+  }
+
+  changeWatchlist() {
+    this.watchlistChange.emit(this.movie.id);
+  }
 }
--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MovieCardComponent } from "../movie-card/movie-card.component";
 
 @Component({
@@ -7,25 +7,52 @@ import { MovieCardComponent } from "../movie-card/movie-card.component";
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
-export class MovieListComponent {
-  movies: any[] = [
-    {
-      id: 1,
-      title: "El Señor de los Anillos",
-      duration: 300,
-      director: "Peter Jackson"
-    },
-    {
-      id: 2,
-      title: "Star Wars",
-      duration: 120,
-      director: "George Lucas"
-    },
-    {
-      id: 3,
-      title: "La Naranja Mecánica",
-      duration: 100,
-      director: "Stanley Kubrick"
-    },
-  ]
+export class MovieListComponent implements OnInit {
+  movies: any[];
+
+   constructor() {
+    console.log("Mensaje desde el constructor");
+    
+    this.movies = [
+      {
+        id: 1,
+        title: "El Señor de los Anillos",
+        duration: 300,
+        director: "Peter Jackson",
+        img: "https://i.blogs.es/049666/el-senor-de-los-anillos-mejor-orden-peliculas/1366_2000.jpg",
+        isWatchlist: false
+      },
+      {
+        id: 2,
+        title: "Star Wars",
+        duration: 120,
+        director: "George Lucas",
+        img: "https://i.blogs.es/8330d9/star-wars-orden-todas-las-peliculas/1366_2000.jpeg",
+        isWatchlist: false
+      },
+      {
+        id: 3,
+        title: "La Naranja Mecánica",
+        duration: 100,
+        director: "Stanley Kubrick",
+        img: "https://images.justwatch.com/poster/307021958/s718/la-naranja-mecanica.jpg",
+        isWatchlist: true
+      },
+    ]
+  }
+
+  ngOnInit(): void {
+    console.log("Mensaje desde el OnInit");    
+  }
+
+  receiveMsg(msg: string) {
+    console.log("El mensaje: " + msg);    
+  }
+
+  toggleWatchlist(id: number) {
+    const selectedMovie = this.movies.find((movie) => movie.id === id);
+    if (selectedMovie) {
+      selectedMovie.isWatchlist = !selectedMovie.isWatchlist;
+    }
+  }
 }
