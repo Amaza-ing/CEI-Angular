20065b0699da3a30b3dc0290572496d55b24f11c
Adrián Maza <adri_mv1@hotmail.com>
2025-02-26 20:17:05 +0100

    reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { ParentComponent } from "./components/parent/parent.component";
 import { MovieListComponent } from "./components/movie/movie-list/movie-list.component";
 import { NewsSectionComponent } from './components/news-section/news-section.component';
+import { UserFormComponent } from "./components/user-form/user-form.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, NewsSectionComponent, ParentComponent, MovieListComponent],
+  imports: [HeaderComponent, NewsSectionComponent, ParentComponent, MovieListComponent, UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.css',
+})
+export class UserFormComponent implements OnInit {
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  ngOnInit(): void {
+    this.userForm.valueChanges.subscribe((value) => {
+      console.log("Cambio detectado: ", value);
+    })
+  }
+
+  submitForm() {
+    console.log(this.userForm.value);
+
+    this.userForm.reset();
+  }
+}
